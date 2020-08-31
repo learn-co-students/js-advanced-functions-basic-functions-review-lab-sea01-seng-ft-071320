c30ac212206b0fc7b4c364f56e4aabd04a50a2f5
afteralec <github email address>
2020-08-31 10:00:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(deco = "*") {
+  return function (adj = "special") {
+    return `You are ${deco}${adj}${deco}!`;
+  };
+}
+
+//Calculator labs
+const Calculator = {};
+
+Calculator.add = function (a, b) {
+  return a + b;
+};
+
+Calculator.subtract = function (a, b) {
+  return a - b;
+};
+
+Calculator.multiply = function (a, b) {
+  return a * b;
+};
+
+Calculator.divide = function (a, b) {
+  return a / b;
+};
+
+// actionApplyer function labs
+function actionApplyer(int, funcList) {
+  for (const func of funcList) {
+    int = func(int);
+  }
+
+  return int;
+}
